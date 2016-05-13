6e2fbef12f6c85ca30a5a3befdf2b5f22bd25040
alina <johanna.smithova@gmail.com>
2016-05-13 21:02:18 +0200

    add activity

---
+++ app/src/main/java/heureka/cz/internal/library/ui/fragments/UserHistoryFragment.java
@@ -0,0 +1,65 @@
+package heureka.cz.internal.library.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import heureka.cz.internal.library.R;
+import heureka.cz.internal.library.application.CodeCamp;
+import heureka.cz.internal.library.helpers.CollectionUtils;
+import heureka.cz.internal.library.repository.Book;
+import heureka.cz.internal.library.rest.ApiDescription;
+import heureka.cz.internal.library.ui.adapters.BookRecyclerAdapter;
+import retrofit2.Retrofit;
+
+/**
+ * Created by Alina on 07/05/2016.
+ */
+public class UserHistoryFragment extends AbstractBookFragment {
+    private String user = "tomas";
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+
+        ((CodeCamp)getActivity().getApplication()).getApplicationComponent().inject(this);
+        apiDescription = new ApiDescription(retrofit);
+
+        return v;
+    }
+
+    @Override
+    protected void callApi() {
+        apiDescription.getUserHistory(user,new ApiDescription.ResponseHandler() {
+            @Override
+            public void onResponse(Object data) {
+                Log.d(TAG, "load books");
+                adapter.setData((ArrayList<Book>) data);
+            }
+
+            @Override
+            public void onFailure() {
+                Log.d(TAG, "fail");
+            }
+        });
+    }
+
+    @Override
+    protected int getTitle() {
+        return R.string.nav_user_history;
+    }
+}
